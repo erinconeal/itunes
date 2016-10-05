e8de3250afbe763ad71b71f2b7049c560d5bccbe
eco <erinconeal@outlook.com>
2016-10-04 19:09:03 -0600

    first commit

--- js/itunesService.js
+++ js/itunesService.js
@@ -4,9 +4,34 @@ angular.module('itunes').service('itunesService', function($http, $q){
 
   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
+  //Note that in the above line, artist is the parameter being passed in.
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
-    //Code here
-    
+  this.getItunes = function(artist) {
+    var deferred = $q.defer();
+    $http({
+      method: 'JSONP',
+      url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+    })
+    .then(function(response) {
+      var parsedResponse = response.data.results;
+      var newArr = [];
+      for (var i = 0; i < parsedResponse.length; i++) {
+        newArr.push({
+          Play: parsedResponse[i].previewUrl,
+          Artist: parsedResponse[i].artistName,
+          Collection: parsedResponse[i].collectionName,
+          AlbumArt: parsedResponse[i].artworkUrl100,
+          Type: parsedResponse[i].kind,
+          Genre: parsedResponse[i].primaryGenreName,
+          CollectionPrice: parsedResponse[i].collectionPrice
+        })
+      }
+      deferred.resolve(newArr);
+      // deferred.resolve(parsedResponse);
+
+    })
+    return deferred.promise;
+  }
+
 });
